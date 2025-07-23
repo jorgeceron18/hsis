ff412f7229cacb1bebb8038d4869aef6e9be861f
Jorge <jorgeceron18@hotmail.com>
2025-07-23 16:42:33 -0500

    Build HTML form and validation for brands page

---
+++ brands.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'bootstrap.php';
+
+
+
+
+$errors = [];
+$success_message = '';
+
+if (isset($_POST['add_new_brand'])) {
+    $validation_result = validate($_POST['new_brand_name'], "Brand");
+    if (is_array($validation_result)) {
+        $errors = $validation_result;
+    } else {
+        $clean_brand_name = $validation_result;
+    }
+}
+
+?>
+<?php
+require_once 'partials/header.php';
+
+
+?>
+
+<form action="brands.php" method="POST">
+    <label for="new_brand_name">New Brand Name:</label>
+    <input type="text" name="new_brand_name" id="new_brand_name"> <br>
+    <input type="submit" name="add_new_brand" value="Add New Brand"> <br>
+
+
+
+</form>
+
+<?php
+require_once 'partials/footer.php';
+
+?>
--- functions.php
+++ functions.php
@@ -1,27 +1,29 @@
 <?php
-function validate_category_name($category)
+
+
+function validate($name, $type)
 {
     $errors = [];
-    $new_category_name = trim($category);
-    if (empty($new_category_name)) {
-        $errors[] = "Category name cannot be empty or just spaces.";
+    $new_name = trim($name);
+    if (empty($new_name)) {
+        $errors[] = "The {$type} name cannot be empty or just spaces.";
     }
 
-    if (strlen($new_category_name) < 3) {
-        $errors[] = "Category name must be at least 3 characters.";
+    if (strlen($new_name) < 3) {
+        $errors[] = "The {$type} name must be at least 3 characters.";
     }
 
-    if (strlen($new_category_name) > 50) {
-        $errors[] = "Category name must be maximum 50 characters.";
+    if (strlen($new_name) > 50) {
+        $errors[] = "The {$type} name must not exceed 50 characters.";
     }
 
-    if (!preg_match("/^[\p{L}\p{N}\s]+$/u", $new_category_name)) {
-        $errors[] = "Category name can only contain letters, numbers, and spaces.";
+    if (!preg_match("/^[\p{L}\p{N}\s]+$/u", $new_name)) {
+        $errors[] = "The {$type} name can only contain letters, numbers, and spaces.";
     }
 
     if (!empty($errors)) {
         return $errors;
     } else {
-        return $clean_new_category_name = preg_replace('/\s+/', ' ', $new_category_name);
+        return  preg_replace('/\s+/', ' ', $new_name);
     }
 }
