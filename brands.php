148acaacac18e219bcaf97383ad4b1eb9acc07e5
Jorge <jorgeceron18@hotmail.com>
2025-07-23 19:00:22 -0500

    Implement Create and Read for brands page

--- brands.php
+++ brands.php
@@ -8,12 +8,38 @@ require_once 'bootstrap.php';
 $errors = [];
 $success_message = '';
 
+try {
+    $stmt = $pdo->query("SELECT * FROM brands ORDER BY brand_name ASC");
+    $brands = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+
+
+    die("Error: Could not fetch brands");
+}
+
+
+
+if (isset($_SESSION['success_message'])) {
+    $success_message = $_SESSION['success_message'];
+    unset($_SESSION['success_message']);
+}
+
 if (isset($_POST['add_new_brand'])) {
     $validation_result = validate($_POST['new_brand_name'], "Brand");
     if (is_array($validation_result)) {
         $errors = $validation_result;
     } else {
         $clean_brand_name = $validation_result;
+        try {
+            $sql = "INSERT INTO brands (brand_name) VALUES (?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$clean_brand_name]);
+            $_SESSION['success_message'] = "The Brand '{$clean_brand_name}' has been added successfully!";
+            header("Location: brands.php");
+            exit;
+        } catch (PDOException $e) {
+            die("Database error: Could not insert brand.");
+        }
     }
 }
 
@@ -29,6 +55,13 @@ if (!empty($errors)) {
     echo "</div>";
 }
 
+if (!empty($success_message)) {
+    echo "<div class= 'success-box'>";
+    echo htmlspecialchars($success_message);
+    echo "</div>";
+}
+
+
 ?>
 
 <form action="brands.php" method="POST">
@@ -39,6 +72,31 @@ if (!empty($errors)) {
 
 
 </form>
+
+
+<?php if (!empty($brands)): ?>
+
+    <h2>Existing Brands </h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Brand Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($brands as $brand): ?>
+                <tr>
+                    <td> <?php echo htmlspecialchars($brand['brand_name']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+
+
+        </tbody>
+
+    </table>
+
+
+<?php endif; ?>
 
 <?php
 require_once 'partials/footer.php';
